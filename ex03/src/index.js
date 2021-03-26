c5ed356ee0c1f6d215169fcefb3d81dad20ef714
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-26 20:23:54 +0100

    Added ex03

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -7,16 +7,15 @@ import './index.css';
 // add the following code below this line
 
 class App extends React.Component {
-    render() {
-        return ( <
-            div className = "App" >
-            <
-            h1 > Hello, Padawans! < /h1> <
-            /div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1 > Hello, Padawans!</h1>
+      </div >
+    )
+  }
 }
 
 // add the following code below this line
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render(< App />, document.getElementById('root'));
