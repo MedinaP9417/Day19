e6ab9ffa378010689bae34a638fb2cf0544e74eb
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-26 23:04:12 +0100

    recovered files

---
+++ ex04/src/index.js
@@ -0,0 +1,40 @@
+// src index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// add the following code below this line
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 > Hello, Padawans!</h1>
+      </div >
+    )
+  }
+}
+
+// add the following code below this line
+
+ReactDOM.render(< App />, document.getElementById('root'));
+
+
+// src/index.js
+
+import React, { Hello, Padawans } from 'react'; // Change this line
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component { // Change this line 
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
